abdee6303c54312b0c9fa0afb452d60b8435b4c2
dhitozarfhan <dhitozarfhan@gmail.com>
2024-06-14 10:00:45 +0700

    void search

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -50,6 +50,12 @@ public:
 		}
 	}
 
+	void search(string element, Node*& parent, Node*& currentNode) {  // Locate the node which will be the parent of the new node
+		// This function searches the current Node of the specified Node as well as the currentNode of its parent
+		currentNode = ROOT;
+		parent = NULL;
+
+
 
 
 
